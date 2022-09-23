730a4434401fc1b53b10562e9e1133d5c56e1da2
shihabmin <shihab.palekar@mintifi.com>
2022-09-23 16:11:30 +0530

    Screen : bank account - step 2

--- src/components/Onboarding/FormContent/BankAccount/BankAcc.js
+++ src/components/Onboarding/FormContent/BankAccount/BankAcc.js
@@ -1,10 +1,38 @@
+import { BankFilled, SyncOutlined, UploadOutlined } from "@ant-design/icons"
+import { Col, Typography, Row } from "antd"
 import "./BankAcc.css"
 
 const BankAcc = () => {
-    return(
+    return (
         <div className="bankacc-main-wrapper">
-            <p className="info-header">Step 2 of 3!</p>
-            <p className="info-sub-header">Verify your primary/ salary account, via one of the options given below.</p>
+            <Row>
+                <Typography className="info-header">Step 2 of 3!</Typography>
+                <Typography className="info-sub-header">Verify your primary/ salary account, via one of the options given below.</Typography>
+            </Row>
+            <div className="add-bank-tabs-wrapper">
+                <Typography className="recommended-tab">RECOMMENDED</Typography>
+                <div className="add-bank-tab">
+                    <SyncOutlined />
+                    <div className="add-bank-tab-text">
+                        <p className="main-text">Account Aggregator</p>
+                        <p className="sub-text">OTP-based bank statement</p>
+                    </div>
+                </div>
+                <div className="add-bank-tab">
+                    <BankFilled />
+                    <div className="add-bank-tab-text">
+                        <p className="main-text">Netbanking</p>
+                        <p className="sub-text">User Id &amp; password</p>
+                    </div>
+                </div>
+                <div className="add-bank-tab">
+                    <UploadOutlined />
+                    <div className="add-bank-tab-text">
+                        <p className="main-text">Bank Statement</p>
+                        <p className="sub-text">Upload a PDF statement</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Onboarding/StaticSideBar/StaticSideBar.js
+++ src/components/Onboarding/StaticSideBar/StaticSideBar.js
@@ -7,12 +7,13 @@ const StaticSideBar = () => {
     return (
         <div className="staticsidebar-main-wrapper">
             <div>
-                <p className="info-header">Simplify your Line-of-credit</p>
+                <p className="info-header">Simplify your</p>
+                <p className="info-header">Line-of-credit</p>
                 <p className="info-sub-header">In 3 easy steps!</p>
 
             </div>
-            <div>
-                <Steps direction="vertical" current={1}>
+            <div className="steps-wrapper">
+                <Steps direction="vertical" size="small" current={1}>
                     <Step title="Personal &amp; GST Details" />
                     <Step title="Bank Account" />
                     <Step title="KYC &amp; Approvals" />
